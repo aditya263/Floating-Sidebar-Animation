b46cc576c20afea0c40ee9d8866830f8826b1fe0
Aditya Ranjan <ranjanaditya263@gmail.com>
2022-01-25 19:56:16 +0530

    Menu widget created

--- lib/menu_page.dart
+++ lib/menu_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 
-class MenuPage extends StatelessWidget{
+class MenuPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-
+  Widget build(BuildContext context) => const Scaffold(
+        backgroundColor: Colors.indigo,
+      );
 }
--- lib/payment_page.dart
+++ lib/payment_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
-class PaymentPage extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+import 'menu_widget.dart';
 
+class PaymentPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text('Payment Page'),
+          leading: MenuWidget(),
+        ),
+      );
 }
