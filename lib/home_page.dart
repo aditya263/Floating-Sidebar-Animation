fa4fe2779abd4b0afaac04987063ec42a94c034c
Aditya Ranjan <ranjanaditya263@gmail.com>
2022-01-25 19:35:43 +0530

    flutter zoom drawer dependency added

---
+++ lib/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sidebar_animation/menu_page.dart';
+import 'package:flutter_sidebar_animation/payment_page.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+
+class HomePageState extends StatefulWidget{
+  @override
+  State<HomePageState> createState() => _HomePageStateState();
+}
+
+class _HomePageStateState extends State<HomePageState> {
+  @override
+  Widget build(BuildContext context) => ZoomDrawer(menuScreen: MenuPage(), mainScreen: PaymentPage());
+}
